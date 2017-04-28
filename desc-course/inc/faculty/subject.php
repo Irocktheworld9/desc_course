c6a47cd764f8687c9e2d0ed85722f320e2aa6257
Sirsendu Gope <Sirsendu Gope>
2017-04-29 01:51:52 +0530

    1st commit of project design with html and css

---
+++ desc-course/inc/faculty/subject.php
@@ -0,0 +1,11 @@
+<?php
+/*
+1.	Details about the subject under the faculty
+2.	Select the course
+3.	Table details under all course
+	i.	Course ID
+	ii.	Course Name
+	iii.Subject ID
+	iv. Subject Name
+	v.	Syllabus*/
+?>
---
+++ desc-course/inc/student/sidebar.php
@@ -0,0 +1,7 @@
+<li><a href="<?php echo HOME_URL ?>student.php?page=semester">Semister</a></li>
+<li><a href="<?php echo HOME_URL ?>student.php?page=subject">Subject</a></li>
+<li><a href="<?php echo HOME_URL ?>student.php?page=marks">Marks</a></li>
+<li class="divider"></li>
+<li><a href="<?php echo HOME_URL ?>student.php?page=profile"><i class="material-icons">person</i>Profile</a></li>
+<li><a href="#"><i class="material-icons">settings</i>Settings</a></li>
+<li><a href="#"><i class="material-icons">signout</i>Sign Out</a></li>
